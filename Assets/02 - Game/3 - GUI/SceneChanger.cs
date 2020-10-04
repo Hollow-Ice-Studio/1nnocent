02e0c5eda133d812563c08d1c3e3679bd4de1040
germano <germanosp11@hotmail.com>
2020-10-04 00:45:22 -0300

    Adicionado tela inicial básica

---
+++ Assets/02 - Game/3 - GUI/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    
+    public void ExitGame()
+    {
+        Debug.Log("exitgame");
+        Application.Quit();
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
