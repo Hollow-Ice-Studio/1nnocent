b1a07f217e106d2a97717fd7d8b238da4f915166
germano <germanosp11@hotmail.com>
2020-10-24 05:21:27 -0300

    Criado trigger de audio
    Será util para lugares com audio pré-scriptado

---
+++ Assets/02 - Game/6 - Audio Trigger Prefabs/AudioTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using TheLiquidFire.Notifications;
+namespace innocent
+{
+    public class AudioTrigger : MonoBehaviour
+    {
+        [SerializeField] AudioClip AudioToPlay;
+        [SerializeField, ReadOnly] string PlayerTag = "Player";
+        bool IsActivated = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!IsActivated && other.tag.Equals(PlayerTag))
+            {
+                this.PostNotification(Notification.AUDIO_PLAY, AudioToPlay);
+                IsActivated = true;
+            }
+        }
+    }
+}
