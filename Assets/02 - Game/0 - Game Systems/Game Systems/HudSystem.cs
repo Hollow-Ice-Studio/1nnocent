bd65bd81aa496318a0c3416d9f39c1acc0e9a0ea
germano <germanosp11@hotmail.com>
2020-10-24 05:25:34 -0300

    Update HudSystem.cs
    Implementação da marca de dano com fade-out

--- Assets/02 - Game/0 - Game Systems/Game Systems/HudSystem.cs
+++ Assets/02 - Game/0 - Game Systems/Game Systems/HudSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using TheLiquidFire.Notifications;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,15 +9,96 @@ namespace innocent
 {
     public class HudSystem : GameSystem
     {
+        [SerializeField]  Text UiTextElement;
         [SerializeField]
-        Text UiTextElement;
-        
-        void Start() => this.AddObserver(WriteHudOnNotification, Notification.HUD_WRITE);
+        RawImage 
+            HurtMarkRawImage,
+            InsanityMarkRawImage;
+        Color
+            HurtMarkColor,
+            InsanityColor;
+
+        void Start()
+        {
+            CacheReferences();
+            AddObservers();
+        }
+        void OnDestroy()
+        {
+            RemoveObservers();
+        }
+
+        void CacheReferences()
+        {
+            HurtMarkColor = HurtMarkRawImage.color;
+            HurtMarkColor.a = 0;
+            HurtMarkRawImage.color = HurtMarkColor;
+            HurtMarkRawImage.gameObject.SetActive(true);
+            InsanityColor = InsanityMarkRawImage.color;
+            InsanityColor.a = 0;
+            InsanityMarkRawImage.color = InsanityColor;
+            InsanityMarkRawImage.gameObject.SetActive(true);
+        }
+
+        void AddObservers()
+        {
+            this.AddObserver(WriteHudOnNotification, Notification.HUD_WRITE);
+            this.AddObserver(DisplayHudMarkOnNotification, Notification.HUD_HURT);
+            this.AddObserver(DisplayInsanityMarkOnNotification, Notification.HUD_INSANITY);
+        }
+
+        void RemoveObservers()
+        {
+            this.RemoveObserver(WriteHudOnNotification, Notification.HUD_WRITE);
+            this.RemoveObserver(DisplayHudMarkOnNotification, Notification.HUD_HURT);
+            this.RemoveObserver(DisplayInsanityMarkOnNotification, Notification.HUD_INSANITY);
+        }
 
         #region Notification Handler
-        void WriteHudOnNotification(object sender, object args) => UiTextElement.text = (string)args;
+        void WriteHudOnNotification(object sender, object args)
+        {
+            UiTextElement.text = (string)args;
+        }
+        void DisplayHudMarkOnNotification(object sender, object args=null)
+        {
+            StartCoroutine(TemporaryHurtDisplay(3));
+        }
+        void DisplayInsanityMarkOnNotification(object sender, object args=null)
+        {
+            StartCoroutine(TemporaryInsanityDisplay(2));
+        }
         #endregion
 
+        #region Custom Methods
+
+        private IEnumerator TemporaryHurtDisplay(float waitTime)
+        {
+            HurtMarkColor.a = 1;
+            float divisor = 5;
+            float dividedTime = (waitTime / divisor);
+            for (float i=0;i < waitTime; i += dividedTime)
+            {
+                HurtMarkColor.a -= (1 / divisor);
+                HurtMarkRawImage.color = HurtMarkColor;
+                yield return new WaitForSeconds(dividedTime);
+            }
+        }
+
+        private IEnumerator TemporaryInsanityDisplay(float waitTime)
+        {
+            InsanityColor.a = 1;
+            float divisor = 5;
+            float dividedTime = (waitTime / divisor);
+            float alpha = 0;
+            for (float i = 0; i < waitTime; i += dividedTime)
+            {
+                alpha += 1 / 5;
+                InsanityColor.a = alpha;
+                InsanityMarkRawImage.color = InsanityColor;
+                yield return new WaitForSeconds(dividedTime);
+            }
+        }
+        #endregion
     }
 
 }
