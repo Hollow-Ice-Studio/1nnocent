4d03c44e25eab2a161ed9775d29a0f2ea4db02c3
Erick Ribeiro Araujo <61511798+Crekpozer@users.noreply.github.com>
2020-11-05 23:36:20 -0300

    Commit que soa como muisca para meus ouvidos.
    Nessa commit temos:
    - A adição dos arquivos de som na pasta do projeto.
    - A implementação de alguns deles.
    - Suor, sangue e lagrimas (minhas, não do jogo)

---
+++ Assets/02 - Game/6 - Audio Trigger Prefabs/Audios/Historia/Scripts/PlaySound1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound1 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider coll)
+    {
+
+        if (coll.gameObject.tag == "Player")
+        {
+            FindObjectOfType<AudioManager>().Play("2aparte");
+        }
+    }
+}
---
+++ Assets/02 - Game/6 - Audio Trigger Prefabs/Audios/Historia/Scripts/PlaySound3.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound3 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider coll)
+    {
+
+        if (coll.gameObject.tag == "Player")
+        {
+            FindObjectOfType<AudioManager>().Play("Perder Acesso");
+        }
+    }
+}
