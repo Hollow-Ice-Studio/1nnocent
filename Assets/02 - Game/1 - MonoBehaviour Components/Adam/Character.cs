be0e766a8de1a693380e6f32f1555074d195bc7b
germano <germanosp11@hotmail.com>
2020-11-06 18:09:08 -0300

    Ajustes para Build
    * Realizado movimentação de alguns arquivos: Scripts que mexem com [MenuItem] devem estar em uma pasta dentro de Assets/Editor.
    * Adicionado automaticamentevários metas de Sky que antes não tinha

--- Assets/02 - Game/1 - MonoBehaviour Components/Adam/Character.cs
+++ Assets/02 - Game/1 - MonoBehaviour Components/Adam/Character.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 
 namespace innocent
 {
-    [RequireComponent(typeof(AnimatorController))]
+    [RequireComponent(typeof(Animator))]
     public class Character : MonoBehaviour
     {
         #region Properties
